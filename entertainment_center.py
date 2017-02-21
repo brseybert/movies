368a729c753226154558453d60c0f15cb0c45711
Brett Seybert <brseybert@fastmail.com>
2017-02-21 06:36:35 -0600

    Edit for Pep8 compliance, using autopep8

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,40 +1,55 @@
 import media
 import fresh_tomatoes
 
-office_space = media.Movie("Office Space",
-                           "A hypnotized man learns the true value of modern
-                            work.",
-                           "http://bit.ly/2m3Acqc",
-                           "https://youtu.be/dMIrlP61Z9s")
+# Office Space
+office_space = media.Movie(
+    "Office Space",
+    "A hypnotized man learns the true value of modern work",
+    "http://bit.ly/2m3Acqc",
+    "https://youtu.be/dMIrlP61Z9s"
+)
 
-the_big_lebowski = media.Movie("The Big Lebowski",
-                               "The laziest man in Los Angeles County finds
-                               himself involved in a kidnapping scheme.",
-                               "http://bit.ly/2l37AJp",
-                               "https://youtu.be/cd-go0oBF4Y")
+# The Big Lebowski
+the_big_lebowski = media.Movie(
+    "The Big Lebowski",
+    "The laziest man in Los Angeles County investigates a kidnapping scheme.",
+    "http://bit.ly/2l37AJp",
+    "https://youtu.be/cd-go0oBF4Y")
 
-blood_diamond = media.Movie("Blood Diamond",
-                            "A crooked arms dealer finds a conscience.",
-                            "http://bit.ly/2lBRpWU",
-                            "https://youtu.be/yknIZsvQjG4")
+# Blood Diamond
+blood_diamond = media.Movie(
+    "Blood Diamond",
+    "A crooked arms dealer finds a conscience.",
+    "http://bit.ly/2lBRpWU",
+    "https://youtu.be/yknIZsvQjG4"
+)
 
-the_professional = media.Movie("The Professional",
-                               "A hitman takes a 9-year-old girl as an
-                               apprentice.",
-                               "http://bit.ly/2lC1naP",
-                               "https://youtu.be/DcsirofJrlM")
+# The Professional
+the_professional = media.Movie(
+    "The Professional",
+    "A hitman takes a 9-year-old girl as an apprentice.",
+    "http://bit.ly/2lC1naP",
+    "https://youtu.be/DcsirofJrlM"
+)
 
-vanilla_sky = media.Movie("Vanilla Sky",
-                          "A horrible accident leads the heir of a publishing
-                          company into the future.",
-                          "http://bit.ly/2l3kcAe",
-                          "https://youtu.be/k09OX40NLUw")
+# Vanilla Sky
+vanilla_sky = media.Movie(
+    "Vanilla Sky",
+    "An accident leads the heir of a publishing company into the future.",
+    "http://bit.ly/2l3kcAe",
+    "https://youtu.be/k09OX40NLUw")
 
-bull_durham = media.Movie("Bull Durham",
-                          "A career minor leaguer chases the all-time homerun
-                          record.",
-                          "http://bit.ly/2mhvWPY",
-                          "https://youtu.be/kqqdEwFz4mU")
+# Bull Durham
+bull_durham = media.Movie(
+    "Bull Durham",
+    "A career minor leaguer chases the all-time homerun record.",
+    "http://bit.ly/2mhvWPY",
+    "https://youtu.be/kqqdEwFz4mU"
+)
 
-movies = [office_space, the_big_lebowski, blood_diamond, the_professional, vanilla_sky, bull_durham]
+# Movies list -- to provide input to fresh_tomatoes.py
+movies = [office_space, the_big_lebowski, blood_diamond, the_professional,
+          vanilla_sky, bull_durham]
+
+# Function call
 fresh_tomatoes.open_movies_page(movies)
