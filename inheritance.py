632b7d8c6d0bd167aaee8617c819d1c0d47fc4d9
Brett Seybert <brseybert@fastmail.com>
2017-02-21 05:55:52 -0600

    Edit for Pep 8 compliance

--- inheritance.py
+++ inheritance.py
@@ -8,6 +8,7 @@ class Parent():
         print("Last Name - "+self.last_name)
         print("Eye Color - "+self.eye_color)
 
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child Constructor Called")
@@ -20,8 +21,7 @@ class Child(Parent):
         print("Number of Toys - "+str(self.number_of_toys))
 
 
-#billy_cyrus = Parent("Cyrus", "blue")
-#billy_cyrus.show_info()
+# billy_cyrus = Parent("Cyrus", "blue")
+# billy_cyrus.show_info()
 miley_cyrus = Child("Cyrus", "blue", 5)
 miley_cyrus.show_info()
-
